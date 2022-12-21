623e0335f6707f6d8f9c88a718dd44331259b039
ilua-vasilev <117224752+ilua-vasilev@users.noreply.github.com>
2022-12-21 17:55:35 +0500

    Add files via upload

---
+++ Алгоритмы/Ptr/7/7.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int n_new;
+	int* ptr = &n;
+
+	std::cout << "Ââåäèòå çíà÷åíèå ïåðåìåííîé n" << std::endl;
+	std::cin >> *ptr;
+
+	std::cout << "Ââåäèòå íîâîå çíà÷åíèå ïåðåìåííîé" << std::endl;
+	std::cin >> n_new;
+
+	*ptr = n_new;
+
+	std::cout << "Èçìåí¸ííîå çíà÷åíèå ïåðåìåííîé ðàâíî " << n << std::endl;
+
+	return 0;
+}
