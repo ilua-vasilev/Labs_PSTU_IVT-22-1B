b9a642ff54306c64951cf5590eeb5365625a594a
ilua-vasilev <117224752+ilua-vasilev@users.noreply.github.com>
2022-12-21 17:30:23 +0500

    Add files via upload

---
+++ Алгоритмы/2.19/2.19.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int N;
+	int S = 0;
+
+	cout << "??????? ?????????? ????????? N" << endl;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++)
+	{
+		if (i % 3 == 0)
+		{
+			S -= i;
+		}
+		else
+		{
+			S += i;
+		}
+
+	}
+	cout << "????? S ?????:" << S << endl;
+
+	return 0;
+}
