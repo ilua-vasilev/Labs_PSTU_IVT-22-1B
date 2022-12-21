84ed0900c6fc3169aa67d3e8d8c5514a51f0094d
ilua-vasilev <117224752+ilua-vasilev@users.noreply.github.com>
2022-12-21 17:55:07 +0500

    Add files via upload

---
+++ Алгоритмы/Ptr/5/5.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int n;
+	int fact = 1;
+	int* f = &fact;
+
+	std::cout << "Ââåäèòå ÷èñëî" << std::endl;
+	std::cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		*f *= i;
+	}
+
+	std::cout << "Ôàêòîðèàë ÷èñëà " << n << " ðàâåí " << fact << std::endl;
+
+	return 0;
+}
