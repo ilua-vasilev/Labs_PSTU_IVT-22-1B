80bf292f0f3c2e4282ca6feb1f6996dc396a4656
ilua-vasilev <117224752+ilua-vasilev@users.noreply.github.com>
2023-02-20 06:11:12 +0500

    Add files via upload

---
+++ LABS-2-semestr/5 lab/5 lab.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip>
+
+void transpose(int matrix[5][5])
+{
+    int t;
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = i; j < 5; ++j)
+        {
+            t = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = t;
+        }
+    }
+}
+
+int main()
+{
+    int m[5][5] =
+    {
+        {1,2,3,4,5},
+        {6,7,8,9,10},
+        {11,12,13,14,15},
+        {16,17,18,19,20},
+        {21,22,23,24,25}
+    };
+
+
+    transpose(m);
+    for (int i = 0; i < 5; ++i)
+    {
+        for (int j = 0; j < 5; ++j) 
+        {
+            std::cout << std::setw(4) << m[i][j];
+        } 
+        std::cout << '\n';
+    }
+
+    return 0;
+}
