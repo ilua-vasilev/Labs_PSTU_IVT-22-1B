8aadd5d8818fba0805525c9b608f51aa3b226418
ilua-vasilev <117224752+ilua-vasilev@users.noreply.github.com>
2023-05-22 13:00:07 +0500

    Update Class 1.cpp

--- LABS-2-semestr/Classes/Class 1/Class 1.cpp
+++ LABS-2-semestr/Classes/Class 1/Class 1.cpp
@@ -1,45 +1,8 @@
 #include<iostream>
 #include<cmath>
+#include<Triangle.h>
 using namespace std;
-class Triangle
-{
-	public:
-	double first;
-	double second;
-	double hipo;
-	void Read()
-	{
-		cout << "Введите первый катет: ";
-		cin >> first;
-		cout << endl << "Введите второй катет: ";
-		cin >>second;
-		Init();
-	}
-	void Init()
-	{
-		if (first == round(first) or second == round(second))
-		{
-			cout << "Введено целое число, программа завершена!" << endl;
-			abort();
-			
-		}
-		else
-		{
-			cout << "Введены дробные числа, считаем..." << endl;
-			hipotenuse();
-		}
 
-	}
-	void hipotenuse()
-	{
-		hipo =sqrt(pow(first, 2) + pow(second, 2));
-	}
-	void show()
-	{
-		cout << endl << "Гипотенуза равна: " << hipo << endl;
-	}
-	
-};
 Triangle make_Triangle( Triangle object)
 {
 	Triangle object1=object;
